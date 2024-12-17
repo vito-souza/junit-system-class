531cfa67d0d08d24ce34113bf84a5c2140963dce
Vítor Souza <vitorlegalvh@gmail.com>
2024-12-17 18:59:52 -0300

    refactor: SystemData enum and SystemTest class

--- src/test/java/refactoring/SystemData.java
+++ src/test/java/refactoring/SystemData.java
@@ -4,20 +4,16 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public enum SystemData {
-	WIN_LINE_SEPARATOR("\r\n"),
-	UNIX_LINE_SEPARATOR("\n"),
-	WIN_FILE_SEPARATOR("\\"),
-	UNIX_FILE_SEPARATOR("/"),
-	WIN_PATH_SEPARATOR(";"),
-	UNIX_PATH_SEPARATOR(":"),
-	DATE_FORMAT("yyyy-MM-dd HH:mm:ss"),
-	PROPERTY_KEY("key"),
-	PROPERTY_VALUE("value"),
+	WIN_LINE_SEPARATOR("\r\n"), UNIX_LINE_SEPARATOR("\n"), WIN_FILE_SEPARATOR("\\"), UNIX_FILE_SEPARATOR("/"),
+	WIN_PATH_SEPARATOR(";"), UNIX_PATH_SEPARATOR(":"), DATE_FORMAT("yyyy-MM-dd HH:mm:ss"), PROPERTY_KEY("key"),
+	PROPERTY_VALUE("value");
 
-	NULL(null);
+	static final ByteArrayOutputStream NEW_OUT = new ByteArrayOutputStream();
+	static final PrintStream ORIGINAL_OUT = System.out;
 
-	public static final ByteArrayOutputStream NEW_OUT = new ByteArrayOutputStream();
-	public static final PrintStream ORIGINAL_OUT = System.out;
+	static final int[] INT_ARRAY = { 0, 1, 2 };
+	static final int[] INT_DESTINATION_ARRAY = new int[3];
+	static final double[] DOUBLE_DESTINATION_ARRAY = new double[3];
 
 	private final String value;
 
--- src/test/java/refactoring/SystemTest.java
+++ src/test/java/refactoring/SystemTest.java
@@ -1,9 +1,15 @@
 package refactoring;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.PrintStream;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class SystemTest {
 
@@ -15,5 +21,42 @@ class SystemTest {
 	@AfterEach
 	public void tearDown() {
 		System.setOut(SystemData.ORIGINAL_OUT);
+	}
+
+	@Test
+	void shouldCopyArrayWhenTypesAreEqual() {
+		System.arraycopy(SystemData.INT_ARRAY, 0, SystemData.INT_DESTINATION_ARRAY, 0, SystemData.INT_ARRAY.length);
+		assertArrayEquals(SystemData.INT_ARRAY, SystemData.INT_DESTINATION_ARRAY);
+	}
+
+	@Test
+	void shouldThrowArrayStoreExceptionWhenTryingToCopyArrayToDifferentTypeDestiny() {
+		assertThrows(ArrayStoreException.class, () -> {
+			System.arraycopy(SystemData.INT_ARRAY, 0, SystemData.DOUBLE_DESTINATION_ARRAY, 0,
+					SystemData.INT_ARRAY.length);
+		});
+	}
+
+	@Test
+	void shouldClearPropertyWhenPropertyIsSet() {
+		System.setProperty(SystemData.PROPERTY_KEY.getValue(), SystemData.PROPERTY_VALUE.getValue());
+		System.clearProperty(SystemData.PROPERTY_KEY.getValue());
+
+		assertNull(System.getProperty(SystemData.PROPERTY_KEY.getValue()));
+	}
+
+	@Test
+	void shouldThrowIllegalArgumentException() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			System.clearProperty("");
+		});
+	}
+
+	@Test
+	void timeAtTheEndShouldBeGreater() {
+		long t0 = System.nanoTime();
+		long t1 = System.nanoTime();
+
+		assertTrue(t1 > t0);
 	}
 }
